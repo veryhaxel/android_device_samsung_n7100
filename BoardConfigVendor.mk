8c511fb8fc184813307b0492ead77c265e4884dd
altan <korukaltan@gmail.com>
2013-07-16 11:21:31 +0300

    Revert "regulated"
    This reverts commit 7ee1ae904690f81a970868fb9f3ac554521cc3fd.

--- BoardConfigVendor.mk
+++ BoardConfigVendor.mk
@@ -1,4 +1,3 @@
-#
 # Copyright (C) 2012 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,22 +11,3 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
-
-# This variable is set first, so it can be overridden
-# by BoardConfigVendor.mk
-
--include device/samsung/smdk4412-common/BoardCommonConfig.mk
-
-# Bluetooth
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/n7100/bluetooth
-
-# Kernel
-TARGET_KERNEL_SOURCE := kernel/samsung/smdk4412
-TARGET_KERNEL_CONFIG := cyanogenmod_n7100_defconfig
-
-# assert
-TARGET_OTA_ASSERT_DEVICE := t03g,n7100,GT-N7100
-
-# inherit from the proprietary version
--include vendor/samsung/n7100/BoardConfigVendor.mk
